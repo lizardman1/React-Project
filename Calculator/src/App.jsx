17badf81e6a0d449640b3e62cfd9a704f5e3f973
EstesJorie <joe.tresise@aol.com>
2025-02-10 19:14:02 +0000

    100225 addition of updated tip calc.

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
+import TestTip from '../pages/TestTipCalculator'
 import { TipCalculator } from '../components/TipCalculator'
+
 
 function App() {
   return (
-    
-  )
+    <TipCalculator />
+
+    /*<Router>
+      <Switch>
+        <Route path="/Test" component={TestTip} />
+      </Switch>
+    </Router>
+  */);
 }
 
 export default App
