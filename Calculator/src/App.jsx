1bf2e53d4c487931a601805690b7a9ddc3ad0a75
JosephB2 <josephbousfield99@gmail.com>
2025-02-10 19:01:01 +0000

    Merge App.jsx changes

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -2,22 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
 import TipButton from '../components/TipButton'
-=======
-import { TipCalculator } from '../components/TipCalculator'
->>>>>>> 859ed28f871148141cf6a66297c07a1cb7ac83a1
 
 function App() {
   return (
-<<<<<<< HEAD
     <>
       <TipButton percent={40} />
       <TipButton percent={50} />
     </>
-=======
-    <TipCalculator />
->>>>>>> 859ed28f871148141cf6a66297c07a1cb7ac83a1
   )
 }
 
