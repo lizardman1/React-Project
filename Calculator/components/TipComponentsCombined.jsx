c6217f36cab9e571913adf864563ed88908a7e5e
Tim Aldred <tim5597@gmail.com>
2025-02-13 19:48:04 +0000

    update ui

--- Calculator/components/TipComponentsCombined.jsx
+++ Calculator/components/TipComponentsCombined.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext } from "react"
-import { TestTipCalculator } from "./TestTipCalculator"
+import { TipCalculator } from "./TipCalculator"
 
 
 export default function TipComponents() {
@@ -10,7 +10,7 @@ export default function TipComponents() {
             <div className='text-4xl text-[00464a] text-center font-bold mb-20 space-y-4'><p>Tip Calculator</p>
             <div className='grid grid-cols-2 items-center max-w-2xl mx-auto rounded-4xl bg-white  border-white border-20 font-normal text-base'>
             <div className="bg-[#00464a]">
-                <TestTipCalculator /> 
+                <TipCalculator /> 
             </div>
 
             <div className="bg-[#00464a] w-full h-full rounded-3xl rounded-bl-none text-white grid place-items-center p-2">
