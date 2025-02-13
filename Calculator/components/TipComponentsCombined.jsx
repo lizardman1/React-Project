03b2e70fdf82909edb77e9397d8767e2b35aa6a6
Tim Aldred <tim5597@gmail.com>
2025-02-13 18:53:34 +0000

    style changes

--- Calculator/components/TipComponentsCombined.jsx
+++ Calculator/components/TipComponentsCombined.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext } from "react"
-import { TestTipCalculator } from "./TestTipCalculator"
+import { TipCalculator } from "./TipCalculator"
 
 
 export default function TipComponents() {
@@ -7,10 +7,10 @@ export default function TipComponents() {
     return (
         <div>
             <div className='w-screen h-screen flex justify-center items-center bg-[#c5e5e8]'>
-            <div className='text-4xl text-[00464a] text-center font-bold mb-20'><p>Tip Calculator 005</p>
+            <div className='text-4xl text-[00464a] text-center font-bold mb-20'><p>Tip Calculator</p>
             <div className='grid grid-cols-2 items-center max-w-2xl mx-auto rounded-4xl bg-white  border-white border-20 font-normal text-base'>
 
-            <TestTipCalculator /> 
+            <TipCalculator /> 
 
             <div className="bg-[#00464a] w-full h-full rounded-xl text-white">
             answer should be here
