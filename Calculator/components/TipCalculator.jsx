e1e1abe09d4ee09883a4ec6cfc3972ea24dd5e17
EstesJorie <joe.tresise@aol.com>
2025-02-13 19:54:30 +0000

    130225 addition of new TipCalculator.jsx

--- Calculator/components/TipCalculator.jsx
+++ Calculator/components/TipCalculator.jsx
@@ -2,29 +2,38 @@ import React, { createContext, useContext, useState} from 'react'
 
 
 export function TipCalculator(){
-    const [amount, setAmount] = useState('');
-    const [persons, setPersons] = useState('1');
-    const [service, setService] = useState('Select');
-    const [total, setTotal] = useState(null);
-    const [showEach, setShowEach] = useState(false);
+  const [amount, setAmount] = useState('');
+  const [persons, setPersons] = useState('1');
+  const [service, setService] = useState('Select');
+  const [total, setTotal] = useState(null);
+  const [tip, setTip] = useState(null);
+  const [showEach, setShowEach] = useState(false);
 
-    const calculateTip = () => {
+  const calculateTip = () => {
 
-        console.log(amount, persons, service);
+      if(isNaN(amount) || isNaN(persons) || isNaN(service)){
+        alert('Please enter valid values');
+        return;
+      }
 
-        if (amount === '' || service === 'Select') {
-          alert('Please enter values');
-          return;
-        }
-    
-        if (persons === '1')
-          setShowEach(false);
-        else
-          setShowEach(true);
-    
-        const CalcTotal = (amount * service) / persons;
-        setTotal(CalcTotal.toFixed(2));
-      };
+      const numPersons = parseInt(persons, 10);
+      const tipPercentage = parseFloat(service);
+
+      if (numPersons === '1')
+        setShowEach(false);
+      else
+        setShowEach(true);
+
+      const totalTip = (amount * tipPercentage);
+      const tipPerPerson = totalTip / numPersons;
+      const totalAmount = parseFloat(amount) + totalTip;
+      const PersonsCalcTotal = totalAmount / numPersons;
+
+      console.log(totalTip, tipPerPerson, totalAmount, PersonsCalcTotal);
+
+      setTotal(totalAmount.toFixed(2));
+      setTip(PersonsCalcTotal.toFixed(2));
+    };
 
     return (
         <div className='p-6 flex flex-col gap-6 max-w-md mx-auto bg-white rounded-br-3xl'>
@@ -61,8 +70,8 @@ export function TipCalculator(){
                 onChange={(e) => setPersons(e.target.value)} />
             </div>
             <button onClick={calculateTip} className='bg-[#dada33] px-3 py-2 rounded-full fomt-semibold hover:bg-[#c2cd5f]'>Calculate</button>
-            {showEach && <div>Each person should pay: {total}</div>}
-            {total && (<div className='tip'><p>Total Tip: ${total}</p> </div>)}
+            {showEach && (<div className='tip'><p>Total: ${total}</p> </div>)}
+            {showEach && persons > 1 && <div>Each person should pay: {tip}</div>}
             </div>
             );
 }
