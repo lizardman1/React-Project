cb904c8f81b1d184098a60535412e12f9662efe7
Tom Parkinson <tomparkinson12@gmail.com>
2025-02-13 17:57:25 +0000

    button works

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -5,18 +5,20 @@ import './App.css'
 import { TipCalculator } from '../components/TipCalculator'
 import { Route, Routes } from 'react-router-dom'
 import TestTip from '../pages/TestTipCalculator'
+import TipButton from '../components/TipButton'
 
 export default function App() {
   return (
     <>
-      <TipButton percent={40} name="tip" />
+      {/* <TipButton percent={40} name="tip" />
       <TipButton percent={50} name="tip" />
     
-    <TipCalculator />
+    <TipCalculator /> */}
 
       <Routes>
         <Route index element={<TipCalculator />} />
         <Route path="/TestTipCalc" element={<TestTip />} />
+        <Route path="/tipbutton" element={<TipButton percent={40}/>} />
       </Routes>
     </>
   )
